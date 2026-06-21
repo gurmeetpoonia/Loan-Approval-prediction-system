7d632ee7b312390df9d1e5d1a7fc6d8aedfa2e36
Gurmeet Punia <gurmeetpunia404@gmail.com>
2026-06-21 20:48:15 +0530

    CODE SUCCESSFULLY

--- loan_appoval_prediction.py
+++ loan_appoval_prediction.py
@@ -4,8 +4,6 @@ from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.pipeline import Pipeline
-
-from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import roc_auc_score
 from sklearn.metrics import classification_report
@@ -44,7 +42,8 @@ X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42
 
 model.fit(X_train,y_train)
 
-y_pred=model.predict(X_test)
+y_proba = model.predict_proba(X_test)[:,1]
+y_pred = (y_proba > 0.38).astype(int)
 
 #print(model.score(X_test,y_test))
 print("Accuracy:", accuracy_score(y_test, y_pred))
@@ -55,9 +54,9 @@ print(cm)
 
 scores = cross_val_score(model, X, y, cv=5)
 print(scores.mean())
-y_proba = model.predict_proba(X_test)[:, 1]
 
-# 📊 ROC-AUC score
+
+#  ROC-AUC score
 auc = roc_auc_score(y_test, y_proba)
 
 print("ROC-AUC Score:", auc)
